ef9a73043207160144088f887d004a69f4b6dbab
kuiper <kuiper@kuiper.dog>
2023-08-22 22:59:53 -0700

    Major refactoring and implement parallel fetching

--- src/main.ts
+++ src/main.ts
@@ -4,51 +4,56 @@ import { events as objektEvents } from './abi/Objekt'
 import { Context, Log, processor } from './processor'
 import axios from 'axios'
 
+axios.defaults.validateStatus = () => { return true }
+
 processor.run(new TypeormDatabase({supportHotBlocks: true}), async (ctx) => {
-    const entities: Map<string, Entity[]> = new Map();
+    const entities: { [key: string]: Entity[] } = {
+        [Collection.name]: [],
+        [Objekt.name]: [],
+        [Transfer.name]: []
+    }
     
     for (let block of ctx.blocks) {
         for (let log of block.logs) {
             if (log.topics[0] === objektEvents.Transfer.topic) {
-                await handleTransfer(ctx, log, entities)
+                const event = objektEvents.Transfer.decode(log)
+                const token = event.tokenId.toString()
+                const transfer = new Transfer({
+                    id: log.id,
+                    objekt: await ctx.store.get(Objekt, token),
+                    to: event.to,
+                    from: event.from,
+                    timestamp: BigInt(log.block.timestamp)
+                })
+                if (!transfer.objekt) {
+                    let objekt = entities[Objekt.name].find(i => i.id === token) as Objekt
+                    if (objekt)
+                        objekt.transfers.push(transfer)
+                    else
+                        entities[Objekt.name].push(new Objekt({id: token, transfers: [transfer]}))
+                }
+                entities[Transfer.name].push(transfer)
             }
         }
     }
 
-    // Entities must be saved in hierarchical order to avoid referencing a nonexistent relation
-    const saveOrder = ["Collection", "Objekt", "Transfer"]
-
-    for (const key of saveOrder) {
-        const entries = entities.get(key)
-        if (entries) {
-            await ctx.store.save(entries)
-            ctx.log.info('Saved ' + entries.length + ' entities of type ' + key)
-        }
-    }
-})
-
-async function handleTransfer(ctx: Context, log: Log, entities: Map<string, Entity[]>) {
-    ctx.log.info("Processing on block " + log.block.height)
-    const event = objektEvents.Transfer.decode(log)
-
-    let objekt = await getEntity(Objekt, event.tokenId.toString(), ctx, entities);
-    if (!objekt) {
-        let res
-        try {
-            res = await axios.get('https://api.cosmo.fans/objekt/v1/token/' + event.tokenId.toString())
-        } catch (err) {
-            ctx.log.warn('Failed to fetch metadata for token ' + event.tokenId.toString())
-            return
+    let objekts: Objekt[] = []
+    const requests = await Promise.all(entities[Objekt.name].map(objekt => {
+        return axios.get('https://api.cosmo.fans/objekt/v1/token/' + objekt.id)
+    }))
+    for (var i = 0; i < requests.length; i++) {
+        if (requests[i].status === 404 || !requests[i].data || requests[i].data.error) {
+            ctx.log.warn('Failed to fetch metadata for Objekt ' + entities[Objekt.name][i].id)
+            continue
         }
 
-        if (!res.data || !res.data.objekt) {
-            ctx.log.warn('Improper metadata for token ' + event.tokenId.toString())
-            return
-        }
-        const metadata = res.data.objekt
+        const entry = entities[Objekt.name][i] as Objekt
+        const metadata = requests[i].data.objekt
+
         const collectionId = metadata.collectionId.toLowerCase().replaceAll(' ', '-')
-
-        let collection = await getEntity(Collection, collectionId, ctx, entities);
+        let collection = await ctx.store.get(Collection, collectionId)
+        if (!collection)
+            collection = entities[Collection.name].find(e => e.id === collectionId) as Collection
         if (!collection) {
             collection = new Collection({
                 id: collectionId,
@@ -62,42 +67,30 @@ async function handleTransfer(ctx: Context, log: Log, entities: Map<string, Enti
                 number: metadata.collectionNo,
                 objekts: []
             })
-            pushEntity(Collection.name, collection, entities)
-            ctx.log.info("Saved collection " + collection.id)
+            entities[Collection.name].push(collection)
         }
 
-        objekt = new Objekt({
-            id: event.tokenId.toString(),
-            collection: collection as Collection,
+        const objekt = new Objekt({
+            id: entry.id,
+            collection: collection,
             serial: metadata.objektNo,
             transfers: []
         })
-        pushEntity(Objekt.name, objekt, entities)
-        ctx.log.info("Saved Objekt " + objekt.id)
+
+        entry.transfers.forEach((transfer => {
+            const transferIndex = entities[Transfer.name].findIndex(t => t.id === transfer.id)
+            if (transferIndex !== -1)
+                (entities[Transfer.name][transferIndex] as Transfer).objekt = objekt
+        }))
+        objekts.push(objekt)
     }
+    entities[Objekt.name] = objekts
 
-    pushEntity(Transfer.name, new Transfer({
-        id: log.id,
-        objekt: objekt as Objekt,
-        from: event.from,
-        to: event.to,
-        timestamp: BigInt(log.block.timestamp)
-    }), entities)
-    ctx.log.info("Saved transfer " + log.id)
-}
-
-async function getEntity<E extends Entity>(entityClass: EntityClass<E>, id: string, ctx: Context, map: Map<string, Entity[]>): Promise<Entity | undefined> {
-    if (map.get(entityClass.name)) {
-        const value = map.get(entityClass.name)?.find((i) => i.id === id)
-        if (value)
-            return value
+    for (const key of Object.keys(entities)) {
+        const entries = entities[key]
+        if (entries.length > 0) {
+            await ctx.store.save(entries)
+            ctx.log.info('Saved ' + entries.length + ' entities of type ' + key)
+        }
     }
-    return await ctx.store.get(entityClass, id)
-}
-
-function pushEntity(type: string, value: Entity, map: Map<string, Entity[]>) {
-    if (map.get(type))
-        map.get(type)?.push(value)
-    else
-        map.set(type, [value])
-}
+})
